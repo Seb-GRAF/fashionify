d2614d4459d0512233674e3f34168c4c8a16687b
MorePog <95282692+MorePog@users.noreply.github.com>
2022-02-06 23:40:40 +0100

    Switched to hash router

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,19 @@ import Shop from "./pages/Shop";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import "../styles/App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="main">
       <Header />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/about" element={<About />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header>
       <div className="left">
-        <a href="/">
+        <a href="#/">
           <h1>Fashionify</h1>
         </a>
       </div>
